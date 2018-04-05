0554a35e059037cf3bf2d037eb9ce5059919ade6
⚡️ KITNIPAN ⚡️ <kitnipan@dezainwerkz.com>
2018-04-05 17:02:52 +0700

    Release

---
+++ popup.js
@@ -0,0 +1,23 @@
+var isOn = false;
+
+function updateIcon(tab) {
+  var text = '';
+  if (isOn) text = 'on';
+  chrome.browserAction.setBadgeText({
+    text: text,
+    tabId: tab.id
+  });
+}
+
+function runScript(tab) {
+  var file = isOn ? 'turnOn.js' :'turnOff.js'
+  chrome.tabs.executeScript(tab.id, {
+    file: file
+  });
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  isOn = !isOn;
+  updateIcon(tab);
+  runScript(tab);
+});
